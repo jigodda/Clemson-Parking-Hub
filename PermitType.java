af8a91d0365557678a8403de563530ec88c85307
jigodda <jigodda@clemson.edu>
2023-04-26 18:17:12 -0400

    removed carpool

--- PermitType.java
+++ PermitType.java
@@ -6,7 +6,7 @@ class PermitType {
     }
 
     public static void permit(){
-        String category = "no";
+        String category = "None";
         System.out.println("What type of permit do you have?");
         Scanner myObj = new Scanner(System.in);
         String permitInput = myObj.nextLine();
@@ -57,5 +57,30 @@ class PermitType {
             System.out.println("Employee 24 Hour Spaces");
         }
         System.out.println("Visitor Spots (9 PM to 7 AM)");
+
+        if (permitInput.equals("Park N Ride") || permitInput.equals("Carpool") || permitInput.equals("Disability Access")) {
+        category = "Student";
+        }
+
+        System.out.println("You have a " + category + " permit.");
+        System.out.println("You can park in these locations at all times:");
+
+        if (permitInput.equals("Park N Ride")) {
+            System.out.println("East (P5, P7, P8)");
+            System.out.println("West (P6)");
+            System.out.println("(Monday - Friday, 7 A.M. - 6 P.M.)");
+            System.out.println("(Monday - Thursday, 7 A.M. - 6 P.M.)");
+        }
+
+        if (permitInput.equals("Carpool")) {
+            System.out.println("Brooks Center");
+            System.out.println("P&A building");
+            System.out.println("Sirrine lot North and South");
+            System.out.println("C -11");
+            System.out.println("E-4");
+            System.out.println("Newman Hall");
+            System.out.println("Employee Spaces (Weekdays, 7:30PM to 7 AM)");
+            System.out.println("Employee Spaces (Weekends, 4:30PM Friday to 7 AM Monday)");
+        }
     }
 }
